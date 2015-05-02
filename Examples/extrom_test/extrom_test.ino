9693dcb6b5a871642d14b53b01ed7a1f6766b50a
Mohit Mohan <mht.mhn@gmail.com>
2015-05-02 18:10:09 +0530

    Better Documentation, Clean Up Code
    Use pseudo random values for extrom_test, Improve Documentation, Remove
    unwanted keywords.

--- Examples/extrom_test/extrom_test.ino
+++ Examples/extrom_test/extrom_test.ino
@@ -1,35 +1,41 @@
 #include <Wire.h>
 #include <extrom.h>
-extrom rom;
-//extrom rom0,rom1(0x51)...rom7(0x57);
-unsigned long wstarttime, wstoptime, rstoptime;
+extrom rom; //Or extrom rom0; default address is 0x50 when nothing is passed
+//extrom rom1(0x51); //Here 0x51 is set as address for object rom1!(A0 pulled to 5V in device)
+//Use: extrom rom0,rom1(0x51)...rom7(0x57); to interface upto 8 devices passing I2C device address
+unsigned long wstarttime, wstoptime, rstoptime;//Timing variables
+int a;//Random data variable
 
-int a = 0;
 void setup() {
-  while (!Serial) {}
+  while (!Serial);//Wait for serial enumeration, Leonardo Only!
   Serial.begin(115200);
 
-
+  //Write random data
   wstarttime = millis();
-  for (unsigned long i = 0; i < 65536 ; i++)
+  for (unsigned long i = 0; i < 65536 ; i++)//For 64KB EEPROM (24LC512k) 0x0000 to 0xFFFF
   {
-    a++;
-    if (a == 256)
-    {
-      a = 0;
-    }
+    a=random(0,255);
     rom.writebyte(i, (byte)a);
+    //rom1.writebyte(i, (byte)a);
     Serial.print("Writing:");
     Serial.print(a);
-    Serial.print(" at:");
+    Serial.print(" at address:");
     Serial.println(i);
   }
   wstoptime = millis();
-  for (unsigned long i = 0; i < 65536 ; i++)
+  
+  //Read data written
+  for (unsigned long i = 0; i < 65536 ; i++)//For 64KB EEPROM (24LC512k) 0x0000 to 0xFFFF
   {
-    Serial.println((int)rom.readbyte(i));
+    Serial.print("Read:");
+    Serial.print((int)rom.readbyte(i));
+    //Serial.print((int)rom1.readbyte(i));
+    Serial.print(" at address:");
+    Serial.println(i);
+
   }
   rstoptime = millis();
+  
   Serial.println("Total Write Time:");
   Serial.println(wstoptime - wstarttime);
   Serial.println("Total Read Time:");
